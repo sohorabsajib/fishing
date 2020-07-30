8c2c16e2e000c9d9d40f052503a69ec07103ebe8
Touhidul Shawan <touhidulshawan@gmail.com>
2020-07-30 15:12:24 +0600

    draw grass and add portion of land

--- main.cpp
+++ main.cpp
@@ -10,6 +10,8 @@
 //#include <windows.h> // uncomment this if you are on windows machine
 #include <GL/glut.h>
 
+#include <cmath>
+
 using namespace std;
 
 //all declarations of my functions
@@ -22,6 +24,10 @@ void init();
 void reshape(GLsizei width, GLsizei height);
 
 void draw();
+
+void drawGrass(float x1, float heightOfGrass);
+
+//void circle(GLfloat rx, GLfloat ry, GLfloat cx, GLfloat cy);
 
 //my global variables
 char input;
@@ -83,16 +89,15 @@ void reshape(GLsizei width, GLsizei height)
     glViewport(0, 0, width, height);
 
     // Set the aspect ratio of the clipping area to match the viewport
-    glMatrixMode(GL_PROJECTION); // To operate on the Projection matrix
-    glLoadIdentity();            // Reset the projection matrix
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+
     if (width >= height)
     {
-        // aspect >= 1, set the height from -1 to 1, with larger width
         gluOrtho2D(-1.0 * aspect, 1.0 * aspect, -1.0, 1.0);
     }
     else
     {
-        // aspect < 1, set the width to -1 to 1, with larger height
         gluOrtho2D(-1.0, 1.0, -1.0 / aspect, 1.0 / aspect);
     }
 }
@@ -139,9 +144,9 @@ void draw()
     glVertex2f(1.29, 0.7);
     glEnd();
 
-//    draw trees
+    //    draw trees
 
-//    1st tree
+    //    1st tree
     glColor3f(0.35, 0.17, 0.1);
     glRectf(-1.4, 0.0, -1.3, 0.4);
     glBegin(GL_TRIANGLES);
@@ -150,41 +155,82 @@ void draw()
     glVertex2f(-1.1, 0.4);
     glVertex2f(-1.35, 0.6);
     glEnd();
-//    2nd tree
+    //    2nd tree
     glColor3f(0.35, 0.17, 0.1);
     glRectf(-0.7, 0.0, -0.6, 0.5);
     glBegin(GL_TRIANGLES);
     glColor3f(0.6, 0.75, 0.06);
     glVertex2f(-0.9, 0.5);
     glVertex2f(-0.4, 0.5);
-    glVertex2f(-0.65,0.8);
+    glVertex2f(-0.65, 0.8);
     glEnd();
-//    3rd tree
+    //    3rd tree
     glColor3f(0.35, 0.17, 0.1);
     glRectf(-0.1, 0.0, 0.0, 0.3);
     glBegin(GL_TRIANGLES);
     glColor3f(0.6, 0.75, 0.06);
     glVertex2f(-0.3, 0.3);
     glVertex2f(0.2, 0.3);
-    glVertex2f(-0.05,0.6);
+    glVertex2f(-0.05, 0.6);
     glEnd();
-//    4th tree
+    //    4th tree
     glColor3f(0.35, 0.17, 0.1);
     glRectf(0.5, 0.0, 0.8, 0.6);
     glBegin(GL_TRIANGLES);
     glColor3f(0.6, 0.75, 0.06);
     glVertex2f(0.3, 0.6);
     glVertex2f(1, 0.6);
-    glVertex2f(0.65,0.88);
+    glVertex2f(0.65, 0.88);
     glEnd();
-//    5th tree
+    //    5th tree
     glColor3f(0.35, 0.17, 0.1);
     glRectf(1.3, 0.0, 1.6, 0.4);
     glBegin(GL_TRIANGLES);
     glColor3f(0.6, 0.75, 0.06);
     glVertex2f(1.1, 0.4);
     glVertex2f(1.8, 0.4);
-    glVertex2f(1.45,0.75);
+    glVertex2f(1.45, 0.75);
     glEnd();
+//    draw grass
+    drawGrass(-1.75, 0.1);
+    drawGrass(-1.73, 0.11);
+//     draw portion of land
+    glColor3f(0.6, 0.75, 0.06);
+    glRectf(-1.75, 0.0, 1.75, -0.15);
     glutSwapBuffers();
+}
+
+void drawGrass(float x1, float heightOfGrass){
+    float x2 = x1 + 0.05;
+    float middlePoint = (x1+x2)/2;
+    float y1 = 0.0;
+    glColor3f(0.28, 0.35, 0.0078);
+    glBegin(GL_TRIANGLES);
+    while (x1 < 1.75){
+        glVertex2f(x1, y1);
+        glVertex2f(x2, y1);
+        glVertex2f(middlePoint, heightOfGrass);
+
+//        update next point to draw continuously
+        x1 += 0.05;
+        x2 = x1 + 0.05;
+        middlePoint = (x1+x2)/2;
+    }
+    glEnd();
+}
+
+//draw circle
+void circle(GLfloat rx, GLfloat ry, GLfloat cx, GLfloat cy)
+{
+    glBegin(GL_TRIANGLE_FAN);
+    glVertex2f(cx, cy);
+
+    for (int i = 0; i <= 100; i++)
+    {
+        float angle = 2 * M_PI * i / 100;
+        float x = rx * cosf(angle);
+        float y = ry * sinf(angle);
+        glVertex2f((x + cx), (y + cy));
+    }
+    glEnd();
 }
